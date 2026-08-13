16ff21919eef7a8bca0b1aacf6b54d3442f92c4e
unknown <harshithaharshi@gmail.com>
2026-08-13 16:20:39 +0530

    chore: remove target build folder from version control

--- target/nikkimart/WEB-INF/classes/seed.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users
-(name, email, password_hash, role)
-VALUES
-('Admin', 'admin@nikkimart.com', 'TEMP_HASH', 'ADMIN'),
-('Seller One', 'seller@nikkimart.com', 'TEMP_HASH', 'SELLER'),
-('Buyer One', 'buyer@nikkimart.com', 'TEMP_HASH', 'BUYER');
